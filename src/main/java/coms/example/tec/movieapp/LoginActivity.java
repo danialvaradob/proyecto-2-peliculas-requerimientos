fe3fb4cead3072db10aa3841d456594d61219d2f
Daniel Alvarado <dab18@hotmail.com>
2018-05-22 14:50:23 -0600

    Database updates, not finished

--- src/main/java/coms/example/tec/movieapp/LoginActivity.java
+++ src/main/java/coms/example/tec/movieapp/LoginActivity.java
@@ -1,13 +1,37 @@
 package coms.example.tec.movieapp;
 
+
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.IOException;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private util.DatabaseHelper mDBHelper;
+    private SQLiteDatabase mDb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mDBHelper = new util.DatabaseHelper(this);
+
+        try {
+            mDBHelper.updateDataBase();
+        } catch (IOException mIOException) {
+            throw new Error("UnableToUpdateDatabase");
+        }
+
+        try {
+            mDb = mDBHelper.getWritableDatabase();
+        } catch (SQLException mSQLException) {
+            throw mSQLException;
+        }
+
+
+
     }
 }
